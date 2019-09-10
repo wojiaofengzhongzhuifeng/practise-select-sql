5a8e03755f38da087c60af9a798926ebfebc26d6
zhangyongwang <20225614@qq.com>
2019-09-10 11:04:45 +0800

    优化 import

--- src/main/java/com/github/hcsp/sql/Sql.java
+++ src/main/java/com/github/hcsp/sql/Sql.java
@@ -3,9 +3,13 @@ package com.github.hcsp.sql;
 
 import java.io.File;
 import java.math.BigDecimal;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.DriverManager;
 
 public class Sql {
 // 用户表：
