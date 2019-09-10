9d3d22d31056d74ab8e874576e3be08a8ee2fda9
zhangyongwang <20225614@qq.com>
2019-09-10 14:57:20 +0800

    完善 javadoc

--- src/main/java/com/github/hcsp/sql/Sql.java
+++ src/main/java/com/github/hcsp/sql/Sql.java
@@ -77,8 +77,10 @@ public class Sql {
      * 题目1：
      * 查询有多少所有用户曾经买过指定的商品
      *
-     * @param goodsId 指定的商品ID
+     * @param goodsId            指定的商品ID
+     * @param databaseConnection 数据库连接
      * @return 有多少用户买过这个商品
+     * @throws SQLException 如果这里有异常
      */
 // 例如，输入goodsId = 1，返回2，因为有2个用户曾经买过商品1。
 // +-----+
@@ -103,9 +105,11 @@ public class Sql {
      * 题目2：
      * 分页查询所有用户，按照ID倒序排列
      *
-     * @param pageNum  第几页，从1开始
-     * @param pageSize 每页有多少个元素
+     * @param pageNum            第几页，从1开始
+     * @param pageSize           每页有多少个元素
+     * @param databaseConnection 数据库连接
      * @return 指定页中的用户
+     * @throws SQLException 如果这里有异常
      */
 // 例如，pageNum = 2, pageSize = 3（每页3个元素，取第二页），则应该返回：
 // +----+----------+------+----------+
@@ -146,6 +150,10 @@ public class Sql {
     /**
      * 题目3：
      * 查询所有的商品及其销售额，按照销售额从大到小排序
+     *
+     * @param databaseConnection 数据库连接
+     * @return 所有商品及其销售额
+     * @throws SQLException 如果这里有异常
      */
 // 预期的结果应该如图所示
 //  +----+--------+------+
@@ -196,6 +204,10 @@ public class Sql {
     /**
      * 题目4：
      * 查询订单信息，只查询用户名、商品名齐全的订单，即INNER JOIN方式
+     *
+     * @param databaseConnection 数据库连接
+     * @return 用户名、商品名齐全的订单
+     * @throws SQLException 如果这里有异常
      */
 // 预期的结果为：
 // +----------+-----------+------------+-------------+
@@ -229,6 +241,10 @@ public class Sql {
     /**
      * 题目5：
      * 查询所有订单信息，哪怕它的用户名、商品名缺失，即LEFT JOIN方式
+     *
+     * @param databaseConnection 数据库连接
+     * @return 所有订单信息，哪怕它的用户名、商品名缺失
+     * @throws SQLException 如果这里有异常
      */
 // 预期的结果为：
 // +----------+-----------+------------+-------------+
