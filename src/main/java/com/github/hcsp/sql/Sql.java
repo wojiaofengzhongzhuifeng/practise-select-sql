0b7ff9a89a7d1ded6a0c650bb277cb316307e77a
zaker-666 <1075151259china@gmail.com>
2020-01-09 16:40:38 +0800

    绝壁过

--- src/main/java/com/github/hcsp/sql/Sql.java
+++ src/main/java/com/github/hcsp/sql/Sql.java
@@ -82,7 +82,7 @@ public class Sql {
      * @param databaseConnection 数据库连接
      * @param goodsId 指定的商品ID
      * @return 有多少用户买过这个商品
-     * @throws SQLException
+     * @throws SQLException 抛出异常
      */
 // 例如，输入goodsId = 1，返回2，因为有2个用户曾经买过商品1。
 // +-----+
@@ -109,7 +109,7 @@ public class Sql {
      * @param pageNum  第几页，从1开始
      * @param pageSize 每页有多少个元素
      * @return 指定页中的用户
-     * @throws SQLException
+     * @throws SQLException 抛出异常
      */
 // 例如，pageNum = 2, pageSize = 3（每页3个元素，取第二页），则应该返回：
 // +----+----------+------+----------+
@@ -157,7 +157,7 @@ public class Sql {
      * 查询所有的商品及其销售额，按照销售额从大到小排序
      * @param databaseConnection 数据库连接
      * @return 预期的GMV结果
-     * @throws SQLException
+     * @throws SQLException 抛出异常
      */
 // 预期的结果应该如图所示
 //  +----+--------+------+
@@ -217,7 +217,7 @@ public class Sql {
      * 查询订单信息，只查询用户名、商品名齐全的订单，即INNER JOIN方式
      * @param databaseConnection 数据库连接
      * @return 预期的结果
-     * @throws SQLException
+     * @throws SQLException 抛出异常
      */
 // 预期的结果为：
 // +----------+-----------+------------+-------------+
@@ -261,7 +261,7 @@ public class Sql {
      * 查询所有订单信息，哪怕它的用户名、商品名缺失，即LEFT JOIN方式
      * @param databaseConnection 数据库连接
      * @return 预期结果
-     * @throws SQLException
+     * @throws SQLException 抛出异常
      */
 // 预期的结果为：
 // +----------+-----------+------------+-------------+
