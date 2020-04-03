313f0d8d353fc8c9138f1081e5c2cd066662fb30
YongJieLiu <617805327@qq.com>
2020-04-03 20:42:35 +0800

    SELECT命令与JDBC使用

--- src/main/java/com/github/hcsp/sql/Sql.java
+++ src/main/java/com/github/hcsp/sql/Sql.java
@@ -73,7 +73,7 @@ public class Sql {
      * 查询有多少所有用户曾经买过指定的商品
      *
      * @param databaseConnection 数据库连接
-     * @param goodsId 指定的商品ID
+     * @param goodsId            指定的商品ID
      * @return 有多少用户买过这个商品
      */
 // 例如，输入goodsId = 1，返回2，因为有2个用户曾经买过商品1。
@@ -98,8 +98,8 @@ public class Sql {
      * 分页查询所有用户，按照ID倒序排列
      *
      * @param databaseConnection 数据库连接
-     * @param pageNum  第几页，从1开始
-     * @param pageSize 每页有多少个元素
+     * @param pageNum            第几页，从1开始
+     * @param pageSize           每页有多少个元素
      * @return 指定页中的用户
      */
 // 例如，pageNum = 2, pageSize = 3（每页3个元素，取第二页），则应该返回：
@@ -143,7 +143,9 @@ public class Sql {
     /**
      * 题目3：
      * 查询所有的商品及其销售额，按照销售额从大到小排序
+     *
      * @param databaseConnection 数据库连接
+     * @return 商品及其销售额
      */
 // 预期的结果应该如图所示
 //  +----+--------+------+
@@ -195,7 +197,9 @@ public class Sql {
     /**
      * 题目4：
      * 查询订单信息，只查询用户名、商品名齐全的订单，即INNER JOIN方式
+     *
      * @param databaseConnection 数据库连接
+     * @return 用户名、商品名齐全的订单
      */
 // 预期的结果为：
 // +----------+-----------+------------+-------------+
@@ -225,7 +229,9 @@ public class Sql {
     /**
      * 题目5：
      * 查询所有订单信息，哪怕它的用户名、商品名缺失，即LEFT JOIN方式
+     *
      * @param databaseConnection 数据库连接
+     * @return 所有的订单
      */
 // 预期的结果为：
 // +----------+-----------+------------+-------------+
