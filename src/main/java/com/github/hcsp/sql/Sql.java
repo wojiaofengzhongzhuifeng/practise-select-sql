a003f4d8760776d240863fd75edde45defaadb02
arsonal <897386402@qq.com>
2020-04-05 21:00:38 +0800

    处理代码检查错误

--- src/main/java/com/github/hcsp/sql/Sql.java
+++ src/main/java/com/github/hcsp/sql/Sql.java
@@ -70,7 +70,7 @@ public class Sql {
     /**
      * 题目1：
      * 查询有多少所有用户曾经买过指定的商品
-     *
+     * @param databaseConnection 数据库连接
      * @param goodsId 指定的商品ID
      * @return 有多少用户买过这个商品
      */
@@ -95,7 +95,7 @@ public class Sql {
     /**
      * 题目2：
      * 分页查询所有用户，按照ID倒序排列
-     *
+     * @param databaseConnection 数据库连接
      * @param pageNum 第几页，从1开始
      * @param pageSize 每页有多少个元素
      * @return 指定页中的用户
@@ -139,6 +139,8 @@ public class Sql {
     /**
      * 题目3：
      * 查询所有的商品及其销售额，按照销售额从大到小排序
+     * @param databaseConnection 数据库连接
+     * @return List<GoodsAndGmv>
      */
 // 预期的结果应该如图所示
 //  +----+--------+------+
@@ -186,6 +188,8 @@ public class Sql {
     /**
      * 题目4：
      * 查询订单信息，只查询用户名、商品名齐全的订单，即INNER JOIN方式
+     * @param databaseConnection 数据库连接
+     * @return List<Order>
      */
 // 预期的结果为：
 // +----------+-----------+------------+-------------+
@@ -215,6 +219,8 @@ public class Sql {
     /**
      * 题目5：
      * 查询所有订单信息，哪怕它的用户名、商品名缺失，即LEFT JOIN方式
+     * @param databaseConnection 数据库连接
+     * @return List<Order>
      */
 // 预期的结果为：
 // +----------+-----------+------------+-------------+
