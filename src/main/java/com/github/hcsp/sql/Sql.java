7c64139dbb4452bbfa4bbac289360700f71397ac
summer <summer_xue.zz@foxmail.com>
2020-07-28 15:48:34 +0800

    select 实战 checkstyle注释修改

--- src/main/java/com/github/hcsp/sql/Sql.java
+++ src/main/java/com/github/hcsp/sql/Sql.java
@@ -76,6 +76,7 @@ public class Sql {
      * 题目1：
      * 查询有多少所有用户曾经买过指定的商品
      *
+     * @Param databaseConnection Connection
      * @param goodsId 指定的商品ID
      * @return 有多少用户买过这个商品
      */
@@ -102,6 +103,7 @@ public class Sql {
      * 题目2：
      * 分页查询所有用户，按照ID倒序排列
      *
+     * @Param databaseConnection Connection
      * @param pageNum  第几页，从1开始
      * @param pageSize 每页有多少个元素
      * @return 指定页中的用户
@@ -149,6 +151,7 @@ public class Sql {
     /**
      * 题目3：
      * 查询所有的商品及其销售额，按照销售额从大到小排序
+     * @Param databaseConnection Connection
      */
 // 预期的结果应该如图所示
 //  +----+--------+------+
@@ -200,6 +203,7 @@ public class Sql {
     /**
      * 题目4：
      * 查询订单信息，只查询用户名、商品名齐全的订单，即INNER JOIN方式
+     * @Param databaseConnection Connection
      */
 // 预期的结果为：
 // +----------+-----------+------------+-------------+
@@ -242,6 +246,7 @@ public class Sql {
     /**
      * 题目5：
      * 查询所有订单信息，哪怕它的用户名、商品名缺失，即LEFT JOIN方式
+     * @Param databaseConnection Connection
      */
 // 预期的结果为：
 // +----------+-----------+------------+-------------+
